cd853e8677388d33fbf3ca0e848f948201e75c65
Sivav <111901038@smail.iitpkd.ac.in>
2021-07-30 10:04:01 +0530

    added y-combinator to template

--- template/$PROBLEM_NAME.cpp
+++ template/$PROBLEM_NAME.cpp
@@ -26,6 +26,26 @@ template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr
 #define dbg(...)
 #endif
 
+template<class Fun>
+class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T>
+    explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+
+    template<class ...Args>
+    decltype(auto) operator()(Args &&...args) {
+        return fun_(std::ref(*this), std::forward<Args>(args)...);
+    }
+};
+
+template<class Fun>
+decltype(auto) y_combinator(Fun &&fun) {
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+
 #define IOS ios::sync_with_stdio(false)
 #define TIE cin.tie(0);cout.tie(0)
 #define fi first
